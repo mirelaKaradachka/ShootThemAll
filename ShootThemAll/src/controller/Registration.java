bce85558ccf227eeac3bd2063bffea8dc0dd988c
Tsvety <cveti_cool@abv.bg>
2015-06-17 22:31:52 +0300

    help to do

--- ShootThemAll/src/controller/Registration.java
+++ ShootThemAll/src/controller/Registration.java
@@ -22,12 +22,7 @@ public class Registration extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
