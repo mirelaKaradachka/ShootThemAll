a55679e4e28a611853177d3a17fbf5e84a0cb9e2
mirelaKaradachka <mirela_karadachka@abv.bg>
2015-06-22 20:05:17 +0300

    changes in create table queries
    changes in create table queries - derby DB

--- ShootThemAll/db/db_create_table.sql
+++ ShootThemAll/db/db_create_table.sql
@@ -1,32 +1,29 @@
-create database shoot_them_all;
-use shoot_them_all;
-
-create table Users(
-id int not null auto_increment ,
+create table APP.Users(
+id int not null GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
 username varchar(45) not null unique,
 password varchar(45) not null,
 email varchar (45) not null,
-notificationAllow  tinyint(1)  not null default 0,
+notificationAllow  int not null check(notificationAllow>=0 and notificationAllow<=1) default 0,
 levelNo smallint not null default 1,
-score int not null,
-choosen_weapon_id int not null default 1, 
-last_activity_on datetime,
+score int not null default 0,
+choosen_weapon_id int not null default 1,
+last_activity_on timestamp, 
 primary key(id)
 );
 
-create table Weapons(
-id int not null auto_increment,
-damage int not null,
-price int not null,
+create table APP.Weapons(
+id int not null GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) ,
+damage int not null default 0,
+price int not null default 0,
 primary key (id)
 ); 
 
-create table UnlockedWeapons( 
-user_id int not null,
+create table APP.UnlockedWeapons( 
+user_id int not null ,
 weapon_id int not null,
 primary key (user_id, weapon_id),
-foreign key (weapon_id) references Weapons (id),
-foreign key (user_id) references Users (id)
+foreign key (weapon_id) references app.Weapons (id),
+foreign key (user_id) references app.Users (id)
 );
 
 
